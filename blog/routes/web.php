80b1cb0e69cf63a43c5d06f27695d57350109514
AZIZ-Soufiane <AZIZ.Soufiane.Solicode@gmail.com>
2025-11-13 11:12:09 +0000

    Form and CSRF

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 
 Route::get('/', function () {
     return view('welcome');
