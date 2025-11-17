6482cbdc2f3f778638f81159d257cfe6ec85cd04
AZIZ-Soufiane <AZIZ.Soufiane.Solicode@gmail.com>
2025-11-17 11:44:51 +0000

    CRUD-Pagination

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,11 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+// Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+// Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/ping', fn() => 'pong');
+Route::get('/', fn() => redirect()->route('articles.index'));
+Route::resource('articles', ArticleController::class)->except(['show']);
